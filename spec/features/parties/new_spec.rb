7ac818290b94e2ecc416db5121f5a6a9bab0d8c2
Chaz Simons <chaz.simons@gmail.com>
2021-12-02 15:38:19 -0800

    Add sad path test for creating a new party

--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe 'New Party' do
       expect(current_path).to eq("/users/#{@user_1.id}")
     end
   end
+
+  describe 'sad path' do
+    before :each do
+      @user_1 = create(:user)
+      @user_2 = create(:user)
+      @user_3 = create(:user)
+      @user_4 = create(:user)
+      @facade = MovieDetailsFacade.new(75, @user_1.id)
+      @movie = @facade.movie
+    end
+
+    it 'does not create a party without a start time', :vcr do
+      visit "/users/#{@user_1.id}/movies/#{@movie.id}/viewing-party/new"
+
+      expect(page).to have_content("Create a Viewing Party for #{@movie.title}!")
+      expect(page).to have_field(:length, with: @movie.runtime)
+      page.check("#{@user_2.id}")
+      page.check("#{@user_3.id}")
+
+      click_button("Create Party")
+      expect(current_path).to eq("/users/#{@user_1.id}/movies/#{@movie.id}/viewing-party/new")
+      expect(page).to have_content('Could not create viewing party.')
+    end
+  end
 end
