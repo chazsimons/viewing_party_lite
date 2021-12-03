6968f2fb4858ea881619ec6196a9f46dcb5b3020
Gregory Fischer <gwhoisj@gmail.com>
2021-12-03 11:34:36 -0500

    Test: coverage -> 100

--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe UserFacade do
   before :each do
-    @user_1 = create(:user)
-    @facade = UserFacade.new(@user_1.id)
+    @user= create(:user)
+    @party = create(:party, creator_id: @user.id)
+    UserParty.create!(user: @user, party: @party)
+    @facade = UserFacade.new(@user)
   end
 
+  describe '#host' do
+    it 'returns the host of a party' do
+      expect(@facade.host(@party)).to eq @user.name
+    end
+  end
 end
